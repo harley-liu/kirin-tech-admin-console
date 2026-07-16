0d85675dfdb7f7dd7e1d506a9b05d64f0c986302
harley-liu <60726352+harley-liu@users.noreply.github.com>
2026-07-16 14:49:04 +1000

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
 import './index.css';
-import { inject } from '@vercel/analytics';
-inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
